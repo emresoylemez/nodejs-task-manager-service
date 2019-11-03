97d76f89efc8fecbc13542aaf59cdafb3fa9ac47
Emre Soylemez <emre.soylemez@tesco.com>
2019-11-03 16:36:56 +0000

    version added

--- src/controllers/user/routes.ts
+++ src/controllers/user/routes.ts
@@ -7,10 +7,13 @@ import validationHandler from "../../middlewares/validationHandler";
 import controllerAdapter from "../../middlewares/controllerAdapter";
 
 const router = Router();
-const v1 = "/v1"
-router.route(v1 + "/signin").post(checkSchema(validations.signin as any), validationHandler(), controllerAdapter(userControllerInstance, "signin"));
-router.route(v1 + "/signout").post(checkSchema(validations.signout as any), validationHandler(), controllerAdapter(userControllerInstance, "signout"));
-router.route(v1 + "/signup").post(checkSchema(validations.signup as any), validationHandler(), controllerAdapter(userControllerInstance, "signup"));
+const v1 = "/v1";
+router.route(v1 + "/signin").post(checkSchema(validations.signin as any), validationHandler(), controllerAdapter(userControllerInstance,
+  "signin"));
+router.route(v1 + "/signout").post(checkSchema(validations.signout as any), validationHandler(), controllerAdapter(userControllerInstance,
+  "signout"));
+router.route(v1 + "/signup").post(checkSchema(validations.signup as any), validationHandler(), controllerAdapter(userControllerInstance,
+  "signup"));
 router.route(v1 + "/validateToken").post(checkSchema(validations.validateToken as any), validationHandler(), controllerAdapter(userControllerInstance,
   "validateToken"));
 
